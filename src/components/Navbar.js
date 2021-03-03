1be0c36a7cc5aa0d72c546319fc9287c432a84c2
ahson-spec <techexe.net@gmail.com>
2021-03-03 07:12:56 +0500

    3-3-2021

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,70 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+  
+    const handleClick = () => setClick(!click);
+    const closemobilemenu = () => setClick(false);
+  
+    const showButton = () => {
+      if (window.innerWidth <= 960) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+    };
+    window.addEventListener('resize', showButton);
+    
     return (
     <>
     <nav className='navbar'>
         <div className='navbar-container'>
          <Link to='/' classname='navbar-logo'>
              INNI BUDGIE <i classname='fab fa-typo3' />
-             </Link> 
-        </div>
+             </Link>
+             <div className='menu-icon' onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'}/> 
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+        <li className='nav-item'>
+        <Link to='/' className='nav-links' onClick={closemobilemenu}>
+        Inni Budgie Habits
+        </Link>
+        </li>
+        <li className='nav-item'>
+        <Link to='/' className='nav-links' onClick={closemobilemenu}>
+        Inni Budgie Products
+        </Link>
+        </li>
+        <li className='nav-item'>
+        <Link to='/' className='nav-links' onClick={closemobilemenu}>
+        Inni Budgie Food
+        </Link>
+        </li>
+        <li className='nav-item'>
+        <Link to='/' className='nav-links' onClick={closemobilemenu}>
+        Inni Budgie Names 
+        </Link>
+        </li>
+        <li className='nav-item'>
+        <Link to='/' className='nav-links-mobile' onClick={closemobilemenu}>
+        Inni Budgie Toys
+        </Link>
+       </li>
+       <li className='nav-item'>
+        <Link to='/' className='nav-links-mobile' onClick={closemobilemenu}>
+        Subscribe My Channel!
+        </Link>
+       </li>
+      </ul>
+               
+        {button && <Button buttonStyle='btn--outline'>Inni Budgie Products</Button>}
+            </div>
     </nav>
    </>
     );
