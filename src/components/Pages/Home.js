f80dd613fc69b5f83416cea89016af65ffeec3db
ahson-spec <techexe.net@gmail.com>
2021-03-12 06:03:03 +0500

    12-3-2021

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Footer from '../Footer';
-import '../../App.css';
 import Videos from '../Videos';
+import '../../App.css';
+
 
 
 
@@ -11,13 +12,7 @@ function Home() {
   return ( 
     <>
      <Footer />
-     <Videos />
-
-     
-    
-     
-     
-     
+     <Videos />   
     </>
   );
 }
