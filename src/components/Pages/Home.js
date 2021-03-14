4b46e9eb9b2b402461e6583ce203d31792a436d2
ahson-spec <techexe.net@gmail.com>
2021-03-14 07:40:15 +0500

    14th-3-2021

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Footer from '../Footer';
 import Videos from '../Videos';
 import '../../App.css';
-
+import ReactPlayer from 'react-player'
 
 function Home() {
   return ( 
     <>
      <Footer />
-     <Videos />   
+     <Videos /> 
+     <ReactPlayer />  
     </>
   );
 }
